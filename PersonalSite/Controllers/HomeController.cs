6b56cb6cfe60e223fd284d9583f20c4983bb30fc
heyaaronschmitt <106359099+heyaaronschmitt@users.noreply.github.com>
2022-07-22 09:12:03 -0500

    Add project files.

---
+++ PersonalSite/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalSite.Models;
+using System.Diagnostics;
+
+namespace PersonalSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PortfolioOverview() 
+        {
+            return View();
+        }
+
+        public IActionResult PortfolioItem() 
+        {
+            return View();
+        }
+
+        public IActionResult Resume() 
+        {
+            return View();
+        }
+
+        public IActionResult Team()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
